6b182675fe011ff92640e3afeb063ea3943067ce
kouichi-kuu <riosoulukou@gmail.com>
2024-02-26 14:32:46 +0900

    コメント追加

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/',(req, res, next)=>{
   const mainInfoContent = JSON.parse(mainInfo).sort((x,y)=>(new Date(y.date).getTime()) - (new Date(x.date).getTime()));
   const slideInfo = mainInfoContent.slice(0,5);
 
-  console.log(mainInfo);
+  //console.log(mainInfo);
   res.render('index',{
     title: 'ポートフォリオ用サンプルサイト',
     mainContent:mainInfoContent,
